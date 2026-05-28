564feaba17779400b217aa8f43312ff33f320666
Jonah Bruzzi <jonahbruzzi@gmail.com>
2026-05-28 00:50:58 -0400

    Fix AMI filter for Ubuntu 24.04 LTS

--- infra/modules/ec2/main.tf
+++ infra/modules/ec2/main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
     name   = "name"
-    values = ["ubuntu-pro-server-*24.04-lts-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
   }
   filter {
     name   = "virtualization-type"
